d3408a8c09b08a9b7b23263227409c450fe72b62
kushshubham <shubham18kushwaha@gmail.com>
2024-12-11 23:43:39 +0530

    created header section

--- clint/src/Pages/SignIn.jsx
+++ clint/src/Pages/SignIn.jsx
@@ -1,5 +1,4 @@
 
-
 export const SignIn = () => {
   return (
     <div>SignIn</div>
--- clint/src/components/Header.jsx
+++ clint/src/components/Header.jsx
@@ -1,7 +1,9 @@
-import {Navbar, TextInput} from 'flowbite-react'
-import {Link} from 'react-router-dom'
+import {Button, Navbar, TextInput} from 'flowbite-react'
+import {Link, useLocation} from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai' 
+import {FaMoon} from 'react-icons/fa'
 export default function Header() {
+    const path = useLocation().pathname
   return (
     <Navbar className='border-b-2'>
         <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -15,6 +17,35 @@ export default function Header() {
             className='hidden lg:inline'
             />
         </form>
+        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+            <AiOutlineSearch/>
+        </Button>
+        <div className='flex gap-2 md:order-2'>
+            <Button className='w-12 h-10 sm:inline' color='gray' pill>
+                <FaMoon/>
+            </Button>
+            <Link to='/signin'>
+            <Button gradientDuoTone='purpleToBlue'>SignIn</Button>
+            </Link>
+        </div>
+
+        <Navbar.Collapse>
+                <Navbar.Link active={path==='/'} as={'div'}>
+                    <Link to='/'>
+                    Home
+                    </Link>
+                </Navbar.Link >
+                <Navbar.Link active={path==='/about'} as={'div'}>
+                    <Link to='/about'>
+                    About
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link active={path==='/product'} as={'div'}>
+                    <Link to='/product'>
+                    Product
+                    </Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
     </Navbar>
   )
 }
