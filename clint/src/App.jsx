6aa5a78a957042406e1abaf4ee12434e94803201
kushshubham <shubham18kushwaha@gmail.com>
2024-12-11 21:59:15 +0530

    routes and pages

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+
+import { BrowserRouter, Routes,  Route } from 'react-router-dom'
+import { Home } from './Pages/Home'
+import { About } from './Pages/About'
+import { Dashboard } from './Pages/Dashboard'
+import { Products } from './Pages/Products'
+import { SignUp } from './Pages/SignUp'
+import { SignIn } from './Pages/SignIn'
+
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-500'>
-      app
-    </div>
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/about' element={<About/>}/>
+  <Route path='/dashboard' element={<Dashboard/>}/>
+  <Route path='/product' element={<Products/>}/>
+  <Route path='signup' element={<SignUp/>}/>
+  <Route path='signin' element={<SignIn/>}/>
+</Routes>
+</BrowserRouter>
   )
 }
