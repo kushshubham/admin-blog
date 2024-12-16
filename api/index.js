744ebb9c525db142d21d6684b461cf89023618d9
kushshubham <shubham18kushwaha@gmail.com>
2024-12-16 20:16:28 +0530

    add a middleware and function to error handle

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.router.js'
 import authRoutes from './routes/auth.router.js'
+import next from 'next'
 const app = express()
 
 dotenv.config()
@@ -21,3 +22,13 @@ app.listen(3000,()=>{
 })
 app.use('/api/user', userRoutes) 
 app.use('/api/auth', authRoutes)
+
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal server error'
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
